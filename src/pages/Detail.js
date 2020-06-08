40e3a692a0f66a5dc6c0fa00fcd428cee693fd17
huyanhtran <trananhhuy1991@gmail.com>
2020-06-08 23:11:21 +0700

    finish UI

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,12 +1,18 @@
 import React, {useEffect, useState} from 'react'
 import { useParams} from "react-router-dom"
-import {Badge, Button, Row, Col,Container} from 'react-bootstrap'
+import {Badge, Button, Row, Col,Container, Spinner} from 'react-bootstrap'
 import Moment from 'react-moment'
 import 'moment-timezone'
+import Login from './Login'
+import { useHistory } from "react-router-dom";
 
 
 
 export default function Detail(props) {
+    let history = useHistory()
+    const loginPage = () => {
+        history.push(`/login`)
+    }
     const { id } = useParams()
 
     let [detailItem, setDetailItem] = useState(null)
@@ -24,42 +30,38 @@ export default function Detail(props) {
     }, [])
 
     if(detailItem === null){
-        return(
-            <div>Loading</div>
-        )
+        return(<div><Spinner animation="border" /></div>)
     }
     return (
         <div className="body-detail">
-        <div className="navbar-detail">
-                <img src={'https://itviec.com/itviec-black-square-facebook.png'} style={{width:'150px'}, {height:'100px'}}/>
+            <div className="navbar-detail">
+                    <img src={'https://itviec.com/itviec-black-square-facebook.png'} style={{width:'150px'}, {height:'100px'}}/>
             </div>
-        <Container className="main-section">
-            <Row>
-                <Col xs={2} className="logo-detail">
-                    <img src={'https://itviec.com/itviec-black-square-facebook.png'} style={{width:'50px'}}/>
-                </Col>
-                <Col xs={10}>
-                    <h2>{detailItem.title}</h2>
-                    <h5>{detailItem.tags.map(tag => (<Badge style={{marginLeft:'10px'}} variant="secondary" className="badge-style">{tag}</Badge>))}</h5>
-                    <div className="box1">
-                        <div>${detailItem.salary}</div>
-                        <div>{detailItem.city} District {detailItem.district}</div>
-                        <div><Moment fromNow>{detailItem.time}</Moment></div>
-                    </div>
-                    <div className="box2">
-                        <h2>Benefits</h2>
-                        <div >{detailItem.benefits.map(benefits => (<li>{benefits}</li>))}</div>
-                    </div>
-                    <div className="box3">
-                        <h2>Description</h2>
-                        <p>{detailItem.description}</p>
-                    </div>
-                    <Button variant="danger" style={{width:'100%'}}>Apply Now</Button>
-                </Col>
-            </Row>
-            
-            
+            <Container className="main-section">
+                <Row>
+                    <Col xs={2} className="logo-detail">
+                        <img src={'https://itviec.com/itviec-black-square-facebook.png'} style={{width:'50px'}}/>
+                    </Col>
+                    <Col xs={10}>
+                        <h2>{detailItem.title}</h2>
+                        <h5>{detailItem.tags.map(tag => (<Badge style={{marginLeft:'10px'}} variant="secondary" className="badge-style">{tag}</Badge>))}</h5>
+                        <div className="box1">
+                            <div>${detailItem.salary}</div>
+                            <div>{detailItem.city} District {detailItem.district}</div>
+                            <div><Moment fromNow>{detailItem.time}</Moment></div>
+                        </div>
+                        <div className="box2">
+                            <h2>Benefits</h2>
+                            <div >{detailItem.benefits.map(benefits => (<li>{benefits}</li>))}</div>
+                        </div>
+                        <div className="box3">
+                            <h2>Description</h2>
+                            <p>{detailItem.description}</p>
+                        </div>
+                        <Button onClick={() => loginPage()      } variant="danger" style={{width:'100%'}} >Apply Now</Button>
+                    </Col>
+                </Row> 
         </Container>
-        </div>
+    </div>
     )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {Tabs, Tab, Nav, Card, Button, Container, Form} from 'react-bootstrap'
+
 
 export default function Login() {
     return (
         <div>
-            <h1>Login page</h1>
+            <div className="navbar-detail">
+                    <img src={'https://itviec.com/itviec-black-square-facebook.png'} style={{width:'150px'}, {height:'100px'}}/>
+            </div>
+            <Container style={{padding: '70px 350px 50px 350px'}}>
+                <Card style={{width:'400px'}}>
+                    <Card.Header>
+                        <Nav variant="tabs" defaultActiveKey="#first">
+                            <Nav.Item>
+                                <Nav.Link href="#first">Create Account</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="#second">Sign In</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Card.Header>
+                    <Card.Body className="body-login">
+                        <Card.Title style={{textAlign:'center'}}>Sign up to apply faster</Card.Title>                
+                        <Button className='button-signup'>
+                            <div><img style={{width:'50px'}} src="https://images.vexels.com/media/users/3/137283/isolated/preview/8ca486faebd822ddf4baf00321b16df1-google-icon-logo-by-vexels.png"/></div>
+                            <div>Sign up to Google</div>
+                        </Button>
+                        <hr></hr>
+                        <Form.Group>
+                            <Form.Control style={{marginBottom:'10px'}}type="email" placeholder="Name" />
+                            <Form.Control style={{marginBottom:'10px'}} type="email" placeholder="Email" />
+                            <Form.Control style={{marginBottom:'10px'}} type="email" placeholder="Password" />
+                        </Form.Group>
+                        <Button className='button-signup'>                        
+                            Sign up to Google
+                        </Button>
+                    </Card.Body>
+                </Card>
+            </Container>
         </div>
     )
 }
